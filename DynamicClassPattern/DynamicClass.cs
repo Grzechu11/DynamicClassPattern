a9166216137e57076de6c4a6add13f009cabd24c
Grzegorz Dworak <gdworak@opgksoftware.com>
2019-03-05 10:42:08 +0100

    add dynamic class pattern example

---
+++ DynamicClassPattern/DynamicClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace DynamicClassPattern
+{
+    public class DynamicClass : DynamicObject
+    {
+        private Dictionary<string, Object> props =
+        new Dictionary<string, object>();
+
+        public DynamicClass() { }
+
+        public override bool TryGetMember(GetMemberBinder binder,
+        out object result)
+        {
+            string name = binder.Name.ToLower();
+            return props.TryGetValue(name, out result);
+
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder,
+        object value)
+        {
+            props[binder.Name.ToLower()] = value;
+            return true;
+        }
+    }
+}
---
+++ DynamicClassPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DynamicClassPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dc = new DynamicClass();
+            dc.hell = 10;
+
+            Console.WriteLine(dc.hell);
+
+            Action<int> ts = new Action<int>(delegate (int i) {
+                Console.WriteLine(i.ToString());
+            });
+
+            dc.rs = ts;
+
+            dc.rs(100);
+
+
+            dynamic ds = new ExpandoObject();
+            ds.val = 20;
+            Console.WriteLine(ds.val);
+            ds.val = "Hello World...";
+            Console.WriteLine(ds.val);
+
+            ds.rs = ts;
+            ds.rs(100);
+
+            Console.Read();
+        }
+    }
+}
